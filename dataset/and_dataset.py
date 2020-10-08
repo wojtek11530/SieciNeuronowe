d57300cfbf09c8c1dd18e31a626b741dee2734f2
wojtek11530 <wojtek19962a32@gmail.com>
2020-10-08 13:01:02 +0200

    feat: add bipolar dataset

--- dataset/and_dataset.py
+++ dataset/and_dataset.py
@@ -5,22 +5,27 @@ import numpy as np
 from utils.random import get_random_float
 
 
-def get_dataset(noise_data_number: int = 10) -> Tuple[np.ndarray, np.array]:
-    x = [[1., 1.], [0., 0.], [0., 1.], [1., 0.]]
-    y = [1, 0, 0, 0]
+def get_dataset(noise_data_number: int = 10, unipolar: bool = True) -> Tuple[np.ndarray, np.array]:
+    if unipolar:
+        zero_val = 0.
+    else:
+        zero_val = -1.
+
+    x = [[1., 1.], [zero_val, zero_val], [zero_val, 1.], [1., zero_val]]
+    y = [1, zero_val, zero_val, zero_val]
 
     limit = 0.05
     for i in range(noise_data_number):
         x.append([get_random_float(1 - limit, 1), get_random_float(1 - limit, 1)])
         y.append(1)
     for i in range(noise_data_number):
-        x.append([get_random_float(0, limit), get_random_float(0, limit)])
-        y.append(0)
+        x.append([get_random_float(zero_val, zero_val + limit), get_random_float(zero_val, zero_val + limit)])
+        y.append(zero_val)
     for i in range(noise_data_number):
-        x.append([get_random_float(0, limit), get_random_float(1 - limit, 1)])
-        y.append(0)
+        x.append([get_random_float(zero_val, zero_val + limit), get_random_float(1 - limit, 1)])
+        y.append(zero_val)
     for i in range(noise_data_number):
-        x.append([get_random_float(1 - limit, 1), get_random_float(0, limit)])
-        y.append(0)
+        x.append([get_random_float(1 - limit, 1), get_random_float(zero_val, zero_val + limit)])
+        y.append(zero_val)
 
     return np.array(x), np.array(y)
--- scripts/train_perceptron.py
+++ scripts/train_perceptron.py
@@ -1,18 +1,30 @@
 from dataset.and_dataset import get_dataset
-from functions.activation_functions import bipolar_activation
+from functions.activation_functions import bipolar_activation, unipolar_activation
 from functions.evaluate_model import evaluate_model
-from functions.loss_functions import bipolar_loss_function
+from functions.loss_functions import bipolar_loss_function, unipolar_loss_function
 from functions.tran_model import train_model
 from models.perceptron import Perceptron
 
 
-def run():
+def run_training(weight_limit: float, unipolar: bool = True):
     learning_rate = 0.1
-    perceptron = Perceptron(2, weight_limit=0.1, loss_fn=bipolar_loss_function, activation_fn=bipolar_activation)
-    dataset = get_dataset(noise_data_number=0)
+    dataset = get_dataset(noise_data_number=0, unipolar=unipolar)
+    if unipolar:
+        print('UNIPOLAR training')
+    else:
+        print('BIPOLAR training')
+    if unipolar:
+        perceptron = Perceptron(2, weight_limit=weight_limit, loss_fn=unipolar_loss_function,
+                                activation_fn=unipolar_activation)
+    else:
+        perceptron = Perceptron(2, weight_limit=weight_limit, loss_fn=bipolar_loss_function,
+                                activation_fn=bipolar_activation)
     train_model(perceptron, dataset, learning_rate)
-    evaluate_model(perceptron, get_dataset(noise_data_number=2))
+    evaluate_model(perceptron, get_dataset(noise_data_number=2, unipolar=unipolar))
 
 
 if __name__ == '__main__':
-    run()
+    weight_lim = 0.1
+    run_training(weight_lim, unipolar=True)
+    run_training(weight_lim, unipolar=False)
+
