fd95b23c6fde0206dad788c9faaabad7995b0dd9
wojtek11530 <wojtek19962a32@gmail.com>
2020-10-16 11:41:45 +0200

    feat(functions): implement activation functions(sigmoid, softmax, ReLu, tanh)

--- functions/activation_functions.py
+++ functions/activation_functions.py
@@ -7,3 +7,19 @@ def unipolar_activation(z: np.ndarray) -> np.ndarray:
 
 def bipolar_activation(z: np.ndarray) -> np.ndarray:
     return np.array(list(map(lambda x: 1 if x > 0 else -1, z)))
+
+
+def sigmoid(z: np.ndarray) -> np.ndarray:
+    return 1.0 / (1.0 + np.exp(-z))
+
+
+def softmax(z: np.ndarray) -> np.ndarray:
+    return np.exp(z) / np.sum(np.exp(z))
+
+
+def tanh(z: np.ndarray) -> np.ndarray:
+    return 2.0 / (1.0 + np.exp(-2 * z)) - 1
+
+
+def relu(z: np.ndarray) -> np.ndarray:
+    return np.maximum(0, z)
