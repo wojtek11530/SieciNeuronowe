729b0e3021b4073347b16a3845b7c76baebc6f54
wojtek11530 <wojtek19962a32@gmail.com>
2020-10-16 10:40:40 +0200

    feat(dataset): create function getting test mnist dataset

--- dataset/mnist_dataset.py
+++ dataset/mnist_dataset.py
@@ -4,9 +4,18 @@ import numpy as np
 
 from mnist import MNIST
 
+_MNIST_DIRECTORY = '.'
+
 
 def get_train_dataset() -> Tuple[np.ndarray, np.array]:
-    mndata = MNIST('.')
+    mndata = MNIST(_MNIST_DIRECTORY)
     mndata.gz = True
     images, labels = mndata.load_training()
+    return np.array(images), np.array(labels)
+
+
+def get_test_dataset() -> Tuple[np.ndarray, np.array]:
+    mndata = MNIST(_MNIST_DIRECTORY)
+    mndata.gz = True
+    images, labels = mndata.load_testing()
     return np.array(images), np.array(labels)
