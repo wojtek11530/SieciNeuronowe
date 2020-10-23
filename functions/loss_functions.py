bd13b0cd89a31149993df4430847c20fa7943a42
wojtek11530 <wojtek19962a32@gmail.com>
2020-10-23 13:45:34 +0200

    feat(functions): add cross netropy loss

--- functions/loss_functions.py
+++ functions/loss_functions.py
@@ -1,9 +1,13 @@
 import numpy as np
 
 
-def loss_function(y_real: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+def diff_loss_function(y_real: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
     return y_real - y_pred
 
 
 def squared_loss_function(y_real: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
     return np.power(y_real - y_pred, 2)
+
+
+def cross_entropy(y_real: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+    return -np.sum(y_real * np.log(y_pred), axis=1, keepdims=False)
