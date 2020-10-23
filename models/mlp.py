1cbe56d5bbda680ae1e32b19ea0e25ae362ebcaf
wojtek11530 <wojtek19962a32@gmail.com>
2020-10-23 13:46:37 +0200

    feat(functions): change MLP running

--- models/mlp.py
+++ models/mlp.py
@@ -1,17 +1,19 @@
 import pickle as pkl
 
-from typing import Optional, Tuple, List, Callable
+from typing import Optional, Tuple, List, Callable, Union
 
 import numpy as np
 
-from functions.activation_functions import sigmoid
+from functions.activation_functions import sigmoid, softmax, tanh, relu
 from models.base import BaseModel
 
 
 class MLP(BaseModel):
     def __init__(self, input_dim: int, output_dim: int, hidden_dims: List[int],
                  init_parameters_sd: float = 1.0,
-                 activation_functions: Optional[List[Callable[[np.ndarray], np.ndarray]]] = None):
+
+                 activation_functions: Optional[List[Union[sigmoid, softmax, tanh, relu]]] = None):
+        self.input_dim = input_dim
         sizes = [input_dim] + hidden_dims + [output_dim]
 
         self.weights = [np.random.normal(0, init_parameters_sd, size=(sizes[i + 1], sizes[i]))
@@ -19,21 +21,24 @@ class MLP(BaseModel):
         self.biases = [np.random.normal(0, init_parameters_sd, size=(sizes[i + 1], 1)) for i in range(len(sizes) - 1)]
 
         if activation_functions is None:
-            self.activation_functions = [sigmoid] * len(self.weights)
+            self.activation_functions = [sigmoid] * (len(self.weights) - 1) + [softmax]
         else:
-            self.activation_functions = activation_functions
+            self.activation_functions = activation_functions + [softmax]
 
-    def forward(self, x: np.ndarray) -> np.ndarray:
+    def forward(self, x: np.ndarray) -> Tuple[np.ndarray, Optional[np.ndarray]]:
         a = x.reshape((-1, 1))
+        z = None
         for weights, bias, fn in zip(self.weights, self.biases, self.activation_functions):
             z = np.dot(weights, a) + bias
             a = fn(z)
-
-        return a
+        return a, z
 
     def update_weight(self, x_set: np.ndarray, y_set: np.ndarray, lr: float, error_margin: float) \
             -> Tuple[bool, Optional[float]]:
         pass
+        # for x, y_real in zip(x_set, y_set):
+        #     z, a = self.forward(x)
+        # deltas = self.loss_fn(y_set, z)
 
     def save_model(self, file_name: str) -> None:
         model_dict = {'weights': self.weights, 'biases': self.biases, 'act_fn': self.activation_functions}
--- scripts/cwiczenie_2/sample_mlp_feedforward.py
+++ scripts/cwiczenie_2/sample_mlp_feedforward.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from dataset.mnist_dataset import load_data_wrapper
 from functions.activation_functions import sigmoid, softmax
 from models.mlp import MLP
@@ -6,14 +8,22 @@ training_data, validation_data, test_data = load_data_wrapper()
 
 mlp_model = MLP(
     input_dim=784, output_dim=10, hidden_dims=[30],
-    activation_functions=[sigmoid, softmax],
+    activation_functions=[sigmoid],
     init_parameters_sd=1
 )
 print(mlp_model)
 print()
 
-for x, y in training_data:
-    y_hat = mlp_model(x)
+x, y = list(zip(*training_data))
+x_data = np.array(x)
+y_data = np.array(y)
+
+limit = 3
+i = 1
+for x, y in zip(x_data, y_data):
+    y_hat, _ = mlp_model(x)
     print(f'y_real:\n{y}')
     print(f'\ny_hat:\n{y_hat}')
-    break
+    i += 1
+    if i > limit:
+        break
