778235ea88c36087aab6ee7243d0ee6827466091
wojtek11530 <wojtek19962a32@gmail.com>
2020-10-24 13:17:25 +0200

    feat(functions): add cross entropy with softmax derivative

--- functions/loss_functions.py
+++ functions/loss_functions.py
@@ -10,4 +10,8 @@ def squared_loss_function(y_real: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
 
 
 def cross_entropy(y_real: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
-    return -np.sum(y_real * np.log(y_pred), axis=1, keepdims=False)
+    return -np.sum(y_real * np.log(y_pred))
+
+
+def cross_entropy_loss_with_softmax_derivative(y_pred: np.ndarray, y_real: np.ndarray) -> np.ndarray:
+    return y_pred - y_real
