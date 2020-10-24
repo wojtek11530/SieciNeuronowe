87bf22e6d5bc8170c51670f70909deed1caa3792
wojtek11530 <wojtek19962a32@gmail.com>
2020-10-24 13:25:50 +0200

    feat(): load MNIST dataset in form of numpy arrays

--- scripts/cwiczenie_2/sample_mlp_feedforward.py
+++ scripts/cwiczenie_2/sample_mlp_feedforward.py
@@ -4,7 +4,7 @@ from dataset.mnist_dataset import load_data_wrapper
 from functions.activation_functions import sigmoid, softmax
 from models.mlp import MLP
 
-training_data, validation_data, test_data = load_data_wrapper()
+x_train, y_train, x_val, y_val, x_test, y_test = load_data_wrapper()
 
 mlp_model = MLP(
     input_dim=784, output_dim=10, hidden_dims=[30],
@@ -14,14 +14,11 @@ mlp_model = MLP(
 print(mlp_model)
 print()
 
-x, y = list(zip(*training_data))
-x_data = np.array(x)
-y_data = np.array(y)
 
 limit = 3
 i = 1
-for x, y in zip(x_data, y_data):
-    y_hat, _ = mlp_model(x)
+for x, y in zip(x_train, y_train):
+    y_hat = mlp_model(x)
     print(f'y_real:\n{y}')
     print(f'\ny_hat:\n{y_hat}')
     i += 1
