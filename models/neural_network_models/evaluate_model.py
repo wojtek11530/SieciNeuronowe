2249e531838c7c1184e3b99cbd856e4faf58f44b
wojtek11530 <wojtek19962a32@gmail.com>
2020-11-01 14:03:12 +0100

    feat: add evaluation of neural network model

---
+++ models/neural_network_models/evaluate_model.py
@@ -0,0 +1,36 @@
+import sys
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+from sklearn.metrics import classification_report, confusion_matrix
+
+from models.neural_network_models.neural_network_base import NeuralNetworkBaseModel
+
+
+def evaluate_model(model: NeuralNetworkBaseModel, x_test: np.ndarray, y_test: np.ndarray):
+    print('\nModel evaluation:')
+    y_label = np.argmax(y_test, axis=1).flatten()
+    y_pred = np.array([model(x) for x in tqdm(x_test, desc=f'Model evaluation', file=sys.stdout)])
+    y_pred_label = np.argmax(y_pred, axis=1).flatten()
+    accuracy = sum(y_label == y_pred_label) / len(y_test)
+    print(f'Aaccuracy={accuracy:.4f}')
+
+    print(classification_report(y_label, y_pred_label))
+
+    cm = confusion_matrix(y_label, y_pred_label)
+    df_cm = pd.DataFrame(cm, index=np.unique(y_label), columns=np.unique(y_label))
+    show_confusion_matrix(df_cm)
+
+
+def show_confusion_matrix(conf_matrix: pd.DataFrame) -> None:
+    hmap = sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues")
+    hmap.yaxis.set_ticklabels(hmap.yaxis.get_ticklabels(), rotation=0, ha='right')
+    hmap.xaxis.set_ticklabels(hmap.xaxis.get_ticklabels(), rotation=0, ha='right')
+    plt.ylabel('Prawdziwa etykieta')
+    plt.xlabel('Predykowana etykieta')
+    plt.tight_layout()
+    plt.show()
--- scripts/cwiczenie_2/train_and_evaluate_mlp.py
+++ scripts/cwiczenie_2/train_and_evaluate_mlp.py
@@ -1,25 +1,54 @@
+from datetime import datetime
+
 from dataset.mnist_dataset import load_data_wrapper
 from functions.activation_functions import sigmoid
+from models.neural_network_models.evaluate_model import evaluate_model
 from models.neural_network_models.mlp import MLP
-from models.neural_network_models.train_model import train_model, plot_losses_during_training, plot_accuracies
+from models.neural_network_models.train_model import train_model
 
 
-def run_training():
+def run_training_and_evaluation():
     x_train, y_train, x_val, y_val, x_test, y_test = load_data_wrapper()
 
+    hidden_dims = [100]
+    activation_functions = [sigmoid, sigmoid]
+    init_parameters_sd = 1
     mlp_model = MLP(
-        input_dim=784, output_dim=10, hidden_dims=[30],
-        activation_functions=[sigmoid],
-        init_parameters_sd=1
+        input_dim=784, output_dim=10, hidden_dims=hidden_dims,
+        activation_functions=activation_functions,
+        init_parameters_sd=init_parameters_sd
     )
     print(mlp_model)
-    learning_rate = 1e-1
+    learning_rate = 2e-1
     batch_size = 50
-    max_epochs = 15
+    max_epochs = 20
 
     train_model(mlp_model, x_train, y_train, lr=learning_rate, batch_size=batch_size, max_epochs=max_epochs,
                 x_val=x_val, y_val=y_val, plot=True, early_stop=True, patience=2)
 
+    file_name = f'mlp_model_{hidden_dims}_sd={init_parameters_sd}' + \
+                f'_lr={learning_rate}_b={batch_size}_{datetime.now().strftime("%m-%d-%Y_%H.%M")}.pkl'
+    mlp_model.save_model(file_name)
+    evaluate_model(mlp_model, x_test, y_test)
+
+
+def evaluate_from_file():
+    file_name = 'mlp_model_[100]_sd=1_lr=0.2_b=50_11-01-2020_12.50.pkl'
+    x_train, y_train, x_val, y_val, x_test, y_test = load_data_wrapper()
+
+    hidden_dims = [100]
+    activation_functions = [sigmoid]
+    init_parameters_sd = 1
+    mlp_model = MLP(
+        input_dim=784, output_dim=10, hidden_dims=hidden_dims,
+        activation_functions=activation_functions,
+        init_parameters_sd=init_parameters_sd
+    )
+    print(mlp_model)
+    mlp_model.load_model(file_name)
+    evaluate_model(mlp_model, x_test, y_test)
+
 
 if __name__ == '__main__':
-    run_training()
+    run_training_and_evaluation()
+    # evaluate_from_file()
