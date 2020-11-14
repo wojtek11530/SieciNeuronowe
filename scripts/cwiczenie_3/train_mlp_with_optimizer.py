483241e5b8ebeee442fc866814bc77c1b98c8971
wojtek11530 <wojtek19962a32@gmail.com>
2020-11-14 12:04:01 +0100

    feat(optimizers): add Adagrad optimizer

--- scripts/cwiczenie_3/train_mlp_with_optimizer.py
+++ scripts/cwiczenie_3/train_mlp_with_optimizer.py
@@ -2,6 +2,7 @@ from dataset.mnist_dataset import load_data_wrapper
 from functions.activation_functions import sigmoid
 from models.neural_network_models.mlp import MLP
 from models.neural_network_models.train_model import train_model
+from optimizers.adagrad import Adagrad
 from optimizers.momentum import Momentum
 from optimizers.nestorov_momentum import NestorovMomentum
 
@@ -17,7 +18,7 @@ def run_training():
         input_dim=784, output_dim=10, hidden_dims=[30],
         activation_functions=[sigmoid],
         init_parameters_sd=1,
-        optimizer=NestorovMomentum(learning_rate=learning_rate, momentum_rate=0.7)
+        optimizer=Adagrad(learning_rate=learning_rate)
     )
 
     print(mlp_model)
